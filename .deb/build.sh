c1bca8764913eff69bc292a9cba8936657c0fdd5
csutorasa <csutorasa@gmail.com>
2023-04-14 17:11:22 +0200

    Fix CI

--- .deb/build.sh
+++ .deb/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
 cp ../icon-metrics icon-metrics/usr/local/bin/
-sed -i "s/Architecture:.*/Architecture: $1/g"
+sed -i "s/Architecture:.*/Architecture: $1/g" icon-metrics/DEBIAN/control
 dpkg-deb --root-owner-group --build icon-metrics
--- .deb/prepare.sh
+++ .deb/prepare.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
 gzip --best -n icon-metrics/usr/share/man/man1/icon-metrics.1
 gzip --best -n icon-metrics/usr/share/doc/icon-metrics/changelog.Debian
