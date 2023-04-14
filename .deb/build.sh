5c6f06e1b58c423112de581a9d7a9cbbfbcb891e
csutorasa <csutorasa@gmail.com>
2023-04-14 17:17:27 +0200

    More CI logs

--- .deb/build.sh
+++ .deb/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo +x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 cp ../icon-metrics icon-metrics/usr/local/bin/
--- .deb/prepare.sh
+++ .deb/prepare.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo +x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 gzip --best -n icon-metrics/usr/share/man/man1/icon-metrics.1
