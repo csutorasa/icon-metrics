056336072c484b581ac14cf5a1be48f6e16b86c0
csutorasa <csutorasa@gmail.com>
2023-04-14 17:22:35 +0200

    Fix missing directory

--- .deb/build.sh
+++ .deb/build.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
+mkdir -p icon-metrics/usr/local/bin/
 cp ../icon-metrics icon-metrics/usr/local/bin/icon-metrics
 sed -i "s/Architecture:.*/Architecture: $1/g" icon-metrics/DEBIAN/control
 dpkg-deb --root-owner-group --build icon-metrics
