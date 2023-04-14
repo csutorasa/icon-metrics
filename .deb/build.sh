822ff8bff6e7550977226d4cee5ddb71a05b788a
csutorasa <csutorasa@gmail.com>
2023-04-14 17:19:47 +0200

    Fixes

--- .deb/build.sh
+++ .deb/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo +x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
-cp ../icon-metrics icon-metrics/usr/local/bin/
+cp ../icon-metrics icon-metrics/usr/local/bin/icon-metrics
 sed -i "s/Architecture:.*/Architecture: $1/g" icon-metrics/DEBIAN/control
 dpkg-deb --root-owner-group --build icon-metrics
