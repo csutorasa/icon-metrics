a6025bde8e016193dcc0932ca00d61193da347d1
Ármin Csutorás <csutorasa@gmail.com>
2025-01-04 22:29:30 +0100

    Update release process

--- .deb/prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
-gzip --best -n icon-metrics/usr/share/man/man1/icon-metrics.1
-gzip --best -n icon-metrics/usr/share/doc/icon-metrics/changelog.Debian
